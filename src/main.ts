cc2733d828f1839f53d7e2e0ce844dbd075cf3a3
AncientPixel-Aron <aron.v.deijnen@onsmail.nl>
2026-09-17 15:44:46 +0200

    Setup github routing

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Import scss here import './assets/styles/main.scss'
+
 const app = createApp(App)
 
 app.use(createPinia())
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,16 @@
+import LandingView from '@/views/LandingView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    component: LandingView,
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
